8fdd39ebbc45b2752302f1abd0874c1aa105a44f
Quentin P <pre.quentin@gmail.com>
2014-02-13 15:36:17 -0500

    [ADD] jsonp support for index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,9 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.json { render :json => @posts, :callback => params[:callback] }
+    end
   end
 
   # GET /posts/1
