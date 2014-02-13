b724f32b3cd91dc9a0ecf6fbd7888c92c765057f
Quentin P <pre.quentin@gmail.com>
2014-02-13 12:43:51 -0500

    [ADD] Rails 4 starter fils

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QupApi::Application.config.secret_key_base = 'dfb3be7cc944b230dac6c160064932b29b05ee4f93b7f22aab9edb69e151a4a1a5942f526dd3a526d4a655b836beb73b5d6c2d55e27322428768696018fb08a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QupApi::Application.config.session_store :cookie_store, key: '_qup-api_session'
