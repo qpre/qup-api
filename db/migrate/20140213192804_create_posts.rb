69f672556568cfd358f24cd0e90b5637a659c968
Quentin P <pre.quentin@gmail.com>
2014-02-13 14:37:46 -0500

    [FIX] Bad row for tables creation

--- db/migrate/20140213192804_create_posts.rb
+++ db/migrate/20140213192804_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :id
       t.references :user, index: true
       t.string :title
       t.datetime :date
