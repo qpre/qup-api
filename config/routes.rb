d2691286aaa7283e0492746dbbd32412c1bbb67a
Quentin P <pre.quentin@gmail.com>
2014-03-16 10:43:23 +0100

    [ADD] Forcing CORS preflight

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ QupApi::Application.routes.draw do
 
   resources :authors
 
+  match "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
