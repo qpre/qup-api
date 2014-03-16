ab9dbddbe5b522ac9d7a643b5f209a9a6abff015
Quentin P <pre.quentin@gmail.com>
2014-03-16 10:01:32 +0100

    [IMP] Handling CORS preflight OPTIONS request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_access_control_headers
+  befor_filter :cors_preflight_check
+  after_filter :set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
 
   def set_access_control_headers
-    headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Expose-Headers'] = 'ETag'
-    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match'
-    headers['Access-Control-Max-Age'] = '86400'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
   end
+  
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      set_access_control_headers
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
+
 end
+
+
