c91b2ba2e3f59e2e11952cabab50ab0525883782
Quentin P <pre.quentin@gmail.com>
2014-03-16 11:05:09 +0100

    TST

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,13 @@ module QupApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', 
+            :headers => :any, 
+            :methods => [:get, :post, :delete, :put, :options]
+      end
+    end
   end
 end
