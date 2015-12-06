e1c7dee88d30261a0a779ff2082e4cc696c57f45
Quentin Pre <pre.quentin@gmail.com>
2015-12-06 09:24:55 +0100

    [ADD] assets compression

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-import express from 'express';
+import express     from 'express';
+import compression from 'compression';
 
 let app = express();
 
+let shouldCompress = function(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+};
+
+app.use(compression({filter: shouldCompress}))
 app.use(express.static('dist'));
 
 let server = app.listen(process.env.PORT || 8080, () => {
