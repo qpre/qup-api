7aa35a3a223279f0577a2e4d223e06a720946814
Quentin Pre <pre.quentin@gmail.com>
2016-01-09 09:52:29 +0100

    [FIX] badly inserted GA script

--- src/lib/google_analytics.js
+++ src/lib/google_analytics.js
@@ -1,27 +1,11 @@
 import {publish, subscribe} from './events';
 import * as Router from './router';
 
-let GA_ID = 'UA-57446905-1';
-
 export function initGoogleAnalytics() {
-  (
-    function(i, s, o, g, r, a, m) {
-      i.GoogleAnalyticsObject = r;
-      i.r = i.r || function() {
-        (i.r.q = i.r.q || []).push(arguments);
-      }, i.r.l = 1 * new Date();
-      a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0];
-      a.async = 1;
-      a.src = g;
-      m.parentNode.insertBefore(a, m);
-    }
-  )(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-  ga('create', GA_ID, 'auto');
+  ga('create', 'UA-57446905-1', 'auto');
   ga('require', 'linkid', 'linkid.js');
 
-  subscribe('route:changed', (currentRoute) => {
-    ga('send', 'pageview', `${currentRoute}`);
+  subscribe('route:changed', () => {
+    ga('send', 'pageview', '/new-page');
   });
 }
